28cfde233a1728527a2536d199da35dbf494ac62
unknown <yamashita-kenngo@outlook.jp>
2019-10-20 23:44:57 +0900

    jwt によるエンドポイントのガード

--- src/shared-func/auth/auth.module.ts
+++ src/shared-func/auth/auth.module.ts
@@ -6,13 +6,14 @@ import { LocalStrategy } from './local.strategy';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt.strategy';
 @Module({
   imports: [UsersModule, PassportModule,
             JwtModule.register({
               secret: jwtConstants.secret,
               signOptions: { expiresIn: '3600s'},
             })],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
