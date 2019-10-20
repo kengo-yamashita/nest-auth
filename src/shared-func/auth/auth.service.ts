163aae7c8b0c2a0ef62f38eabb70ff2865ee1546
unknown <yamashita-kenngo@outlook.jp>
2019-10-20 16:15:42 +0900

    認証サービスとユーザーサービス追加

--- src/shared-func/auth/auth.service.ts
+++ src/shared-func/auth/auth.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { User } from 'src/model/user.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly usersService: UsersService) {}
+
+    async validateUser(username: string, pass: string): Promise<any> {
+        const user = await this.usersService.findOne(username);
+        if (user && user.password === pass) {
+            const { password, ...result } = user;
+            return result;
+        }
+        return null;
+    }
+}
--- src/shared-func/users/users.service.ts
+++ src/shared-func/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository} from 'typeorm';
+import { User } from '../../model/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+    ) {}
+
+    findOne(username: string): Promise<User> {
+        return this.userRepository.findOne({where: {name: username}});
+    }
+}
