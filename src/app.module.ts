3b1a06dc6eb06cfb584c16cb0858b02fd72fd70b
unknown <yamashita-kenngo@outlook.jp>
2019-10-20 14:02:34 +0900

    ユーザーモジュール作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './shared-func/auth/auth.module';
+import { UsersModule } from './shared-func/users/users.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
